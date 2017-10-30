da23d7ff6119538fc6adeb039c22d916436b285e
githubOnik <onikitchuk.prog@gmail.com>
2017-10-30 22:16:14 +0200

    Modif HowManyWords

--- Homework/HowManyWords/HowManyWords.c
+++ Homework/HowManyWords/HowManyWords.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define SIZE 50
+#define SIZE 500
 
 int main() {
 
@@ -9,11 +9,10 @@ int main() {
         printf("Enter the string of words:\n");
         gets(enteredString);
         for(i = 0; i < strlen(enteredString);i++){
-            if(isalpha(enteredString[i]) > 0 && isalpha(enteredString[i - 1]) == 0){
+            if(isalpha(enteredString[i]) > 0 && isalpha(enteredString[i - 1]) == 0 ){
             countOfWords++;
             }
         }
         printf("\nCount of words = %d\n", countOfWords);
-
     return 0;
 }
