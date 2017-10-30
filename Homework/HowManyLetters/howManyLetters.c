18ee5a7cefb0d81cb86a8437f0915cd0546886bc
githubOnik <onikitchuk.prog@gmail.com>
2017-10-31 00:36:39 +0200

    Formatting HowManyLetters

--- Homework/HowManyLetters/howManyLetters.c
+++ Homework/HowManyLetters/howManyLetters.c
@@ -1,66 +1,77 @@
 #include <stdio.h>
+#define SIZE 100
 
-#define SIZE_COUNT 26
-#define SIZE_ENTERED_STRING 50
+int main() {
 
+    int i,j;
+    int countOfLetter[SIZE];
+    int temporary = 0;
+    int temporaryLetter = 0;
 
-int main(){
+    char enteredStr[SIZE];
+    char lettStr[SIZE];
 
-char alphabet[SIZE_COUNT] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'};
-char letterOfString[SIZE_ENTERED_STRING];
-char temporary = 0;
-char enteredString[SIZE_ENTERED_STRING];
+        printf("Enter some words:\n");
+        gets(enteredStr);
 
-int k = 0;
-int i = 0;
-int j = 0;
-int n = 0;
-int countLetters[SIZE_COUNT] = {0};
-
-
-    printf("Enter the word:\n");
-    gets(enteredString);
-    for(i,j; enteredString[i] != '\0'; i++){
-            if(enteredString[i] != ' '){
-                    letterOfString[j++] = enteredString[i];
-                    letterOfString[j] = '\0';
+        for(i = 0; i < SIZE; i++){
+            countOfLetter[i] = 0;
+        }
+            int tempCount = 0;
+        for(i = 0; i < enteredStr[i] != '\0'; i++){
+            char c = enteredStr[i];
+            if(c == ' '){
+                continue;
+            }
+            int varTest = 0;
+            for(j = 0; j < tempCount; j++){
+                if(lettStr[j] == c){
+                    varTest = 1;
                 }
-        }
+            }
+            if(varTest == 0){
+                lettStr[tempCount] = c;
+                tempCount++;
+            }
 
-    for(k = 0;k < strlen(letterOfString);k++){
-        for(i = 0; i < SIZE_COUNT; i++){
-            if(alphabet[i] == letterOfString[k])
-            {
-                countLetters[i]++;
-                break;
+        }
+        lettStr[tempCount] = 0;
+
+      //  printf("\n%s", lettStr);
+
+        printf(" \n Unsorted list:\n");
+        for(i = 0; i < strlen(lettStr); i++){
+            for(j = 0; j < strlen(enteredStr); j++){
+                if(lettStr[i] == enteredStr[j]){
+                    countOfLetter[i]++;
+                }
+            }
+            printf("%5c - %d\n", lettStr[i], countOfLetter[i]);
+        }
+        printf("\n");
+        for(i = 0; i <= strlen(lettStr); i++){
+            for(j = 0; j <= strlen(lettStr); j++){
+                if(countOfLetter[j] < countOfLetter[j + 1]){
+                    temporary = countOfLetter[j];
+                    countOfLetter[j] = countOfLetter[j + 1];
+                    countOfLetter[j + 1] = temporary;
+
+                    temporaryLetter = lettStr[j];
+                    lettStr[j] = lettStr[j + 1];
+                    lettStr[j + 1] = temporaryLetter;
+                }
+            }
+
+        }
+            printf("\n");
+            printf(" Sorted list:\n");
+            int m;
+        for(m = 0; m <= strlen(lettStr); m++){
+            if(countOfLetter[m] != 0){
+                printf("%5c - %d\n", lettStr[m], countOfLetter[m]);
             }
         }
+
+
+        return 0;
     }
-    printf("\n");
-
-    for(i = 0; i < SIZE_COUNT; i++){
-        if(countLetters[i] > 0){
-            printf("%c - %d \n",alphabet[i], countLetters[i]);
-        }
-    }
-    printf("\n");
-     for(i = 0; i < SIZE_COUNT; i++){
-        for(j = 0; j < SIZE_COUNT; j++){
-            if(countLetters[j] < countLetters[j + 1]){
-                temporary = countLetters[j];
-                countLetters[j] = countLetters[j + 1];
-                countLetters[j + 1] = temporary;
-            }
-        }
-    }
-    for(n = 1; n <= SIZE_COUNT; n++){
-            if(countLetters[n] != 0){
-                printf("%d ", countLetters[n]);
-            }
-    }
-
-    return 0;
-}
-
-
-
