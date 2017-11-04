e0d841d291a1132f39dfc937a1c9aebbe424d81a
githubOnik <onikitchuk.prog@gmail.com>
2017-11-04 16:55:05 +0200

    Add HowManyWords-function

---
+++ Homework/Function/HowManyWords-function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define SIZE 500
+
+
+char getString(char* enteredString){
+        printf("Enter the string of words:\n");
+        gets(enteredString);
+}
+
+int main() {
+
+    int i = 0,countOfWords = 0;
+    char enteredString[SIZE];
+
+        getString(enteredString);
+
+        for(i = 0; i < strlen(enteredString);i++){
+            if(isalpha(enteredString[i]) > 0 && isalpha(enteredString[i - 1]) == 0 ){
+            countOfWords++;
+            }
+        }
+        printf("\nCount of words = %d\n", countOfWords);
+    return 0;
+}
