7d4740abc7c6c4977aea3b7743ff90ef5e7fa928
githubOnik <onikitchuk.prog@gmail.com>
2017-11-21 18:52:34 +0200

    Formatting Zoo

--- Homework/Zoo/zoo.cpp
+++ Homework/Zoo/zoo.cpp
@@ -27,23 +27,24 @@ public:
 class Zoo{
 public:
     Zoo(){
-        char* name = new char[20];
-        char* kind = new char[20];
+
         int health = 100;
         int pawsNum = 4;
 
+         for(int i = 0; i < 2; i++){
 
-         for(int i = 0;i < 50;i++){
-
-            cout<<"Enter the name of animal"<<endl;
-            cin.getline(name,20);
-            cout<<"Enter the kind of animal"<<endl;
-            cin.getline(kind,20);
-            cout<<""<<endl;
+            char* name = new char[20];
+            char* kind = new char[20];
 
             animals[i] = new Animal(name, kind, health, pawsNum);
 
-            cout<<"------------------------------------------------------"<<endl;
+            cout<<"Enter the name of animal"<<endl;
+            cin.getline(name, 20);
+            cout<<"Enter the kind of animal"<<endl;
+            cin.getline(kind, 20);
+            cout<<""<<endl;
+
+            cout<<"---------------------------------------------"<<endl;
             cout<<"Add new animal. name = "
             <<animals[i]->name
             <<" kind = "<<animals[i]->kind
@@ -51,9 +52,8 @@ public:
             <<" pawsNum = "<<animals[i]->pawsNum
             <<endl;
             cout<<""<<endl;
+
         }
-        delete []name;
-        delete []kind;
     }
     Animal* animals[50];
 };
